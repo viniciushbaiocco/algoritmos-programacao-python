4d9ef6d2ba3c446f5ec3dede6390f5c94ce2fbbc
Vinicius Hegues Baiocco <vinicius.baiocco@gmail.com>
2026-03-09 14:58:38 -0300

    primeiro commit da aula de algoritmos de programação

---
+++ aula-03-selecao.py
@@ -0,0 +1,87 @@
+## exercicio 1
+##n = int(input("numero: "))
+##if n%2 == 0:
+##    print("numero é par")
+##else:
+##    print ("o numero é impar")
+
+## exercicio 2
+##n = int(input("numero: "))
+##if n%5 == 0:
+##    print("numero é multiplo de 5")
+##else:
+##    print("numero não multiplo de 5")
+
+## exercicio 3
+##A = int(input("numero: "))
+##B = int(input("numero B:"))
+
+##if A == B:
+##    print(A+B)
+##else:
+##    print(A*B)
+
+## exercicio 4
+##n = int(input("salario: "))
+
+##if n < 500:
+##    print(n * 1.15)
+##else:       
+##    if n >= 500 and n < 1000:
+##        print(n * 1.10)
+##    else:
+##        print(n * 1.05)
+
+## exercicio 5
+##n1 = int(input("numero: "))
+##n2 = int(input("numero2: "))
+
+##if n1%n2 == 0:
+##    print ("1° digito é divisivel")
+##else:
+##    print ("1° digito não é divisivel")
+
+#exercicio 6
+##n = int(input("preço: "))
+
+##if n > 100:
+##    print ("o preço se mantem")
+##else:
+##    print(n * 1.10)
+
+## exercicio 7
+##n = int(input("senha: "))
+
+##if n == 2026:
+##    print ("logado")
+##else:
+##    print ("erro")
+
+## exercicio 8
+##n = int(input("salario bruto: "))
+##prestacao = int(input("prestação: "))
+
+##if prestacao > n * 0.30:
+##    print("negado")
+##else:
+##    tempo_de_servico = int(input("digite o tempo em anos: "))
+##    if prestacao <= n * 0.30 and tempo_de_servico >= 2:
+##        print("aprovado com bonus")
+##    else:
+##        if tempo_de_servico < 2 and prestacao <= n * 0.30:
+##           print("aprovado")
+
+## exercicio 9
+##idade = int(input("idade: "))
+##if idade >= 18:
+##    peso = int(input("peso(kg): "))
+##    if peso > 80:
+##        print("categoria peso pesado")
+##        print("peso médio")
+##    else:
+##        print("peso inválido")
+##else:
+##    print("categoria juvenil")
+
+## exercicio 10
+
